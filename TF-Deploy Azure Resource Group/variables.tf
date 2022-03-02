5d86f833529449b2b498d2d3dc81a11a94c50ddc
Anthony Mashford <anthony@mashfords.com>
2022-03-02 22:12:03 +0000

    Initial commit

---
+++ TF-Deploy Azure Resource Group/variables.tf
@@ -0,0 +1,24 @@
+variable "rg_name_1" {
+  description = "Resource Group 1 Name"
+  default     = ""
+}
+
+variable "region_1" {
+  description = "Azure Region"
+  default     = ""
+}
+
+variable "tag_environment" {
+  description = "Resource Group Tags"
+  default     = ""
+}
+
+variable "tag_createdby" {
+  description = "Resource Group Tags"
+  default     = ""
+}
+
+variable "tag_createdwith" {
+  description = "Resource Group Tags"
+  default     = ""
+}

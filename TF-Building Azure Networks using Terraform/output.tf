4ac67d3c392b9f329fcffa60ba7b468fb37823be
Anthony Mashford <anthony@mashfords.com>
2022-03-09 19:18:54 +0000

    Updated

--- TF-Building Azure Networks using Terraform/output.tf
+++ TF-Building Azure Networks using Terraform/output.tf
@@ -1,3 +1,7 @@
-output "resource_group_name" {
+output "resource_group_name_1" {
     value = azurerm_resource_group.rg_1.name
+}
+
+output "resource_group_name_2" {
+    value = azurerm_resource_group.rg_2.name
 }

f7c1b237e724f81db13e25bcbc4ac8579ce06435
Anthony Mashford <anthony@mashfords.com>
2022-04-05 14:34:38 +0100

    Updates

---
+++ TF-Creating an Azure NetApp Files volume using Terraform/anf_capacity_pools.tf
@@ -0,0 +1,35 @@
+# Create Azure NetApp Files Capacity Pool 1
+resource "azurerm_netapp_pool" "anf_cap_1" {
+    provider            = azurerm.demosandbox01
+    name                = "cap-${var.use}-${var.region_1}-${random_string.rg_random_1.result}"
+    account_name        = azurerm_netapp_account.anf_acc_1.name
+    location            = var.region_1
+    resource_group_name = azurerm_resource_group.rg_1.name
+    service_level       = var.cap_pool_tier
+    size_in_tb          = var.cap_pool_size
+    qos_type            = var.qos_type
+
+  tags = {
+    Environment = var.tag_environment
+    CreatedBy   = var.tag_createdby
+    CreatedWith = var.tag_createdwith
+  }
+}
+
+# Create Azure NetApp Files Capacity Pool 2
+resource "azurerm_netapp_pool" "anf_cap_2" {
+    provider            = azurerm.demosandbox01
+    name                = "cap-${var.use}-${var.region_2}-${random_string.rg_random_2.result}"
+    account_name        = azurerm_netapp_account.anf_acc_2.name
+    location            = var.region_2
+    resource_group_name = azurerm_resource_group.rg_2.name
+    service_level       = var.cap_pool_tier
+    size_in_tb          = var.cap_pool_size
+    qos_type            = var.qos_type
+
+  tags = {
+    Environment = var.tag_environment
+    CreatedBy   = var.tag_createdby
+    CreatedWith = var.tag_createdwith
+  }
+}
